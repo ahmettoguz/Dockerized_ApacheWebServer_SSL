d8e405dee737321c0e0d35ce6dbc12f04cf6a3ba
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-10-17 02:35:50 +0300

    Php_Mysql_PhpMyAdmin operations done.

--- Dynamic_Php_MySql_DigitalOcean/php/index.php
+++ Dynamic_Php_MySql_DigitalOcean/php/index.php
@@ -1,18 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php
+require_once __DIR__ . "/assets/controller/sql_functions.php";
+header("Content-Type: application/json");
 
-<h1>
-    <?php
-        $name = getenv("MY_NAME");
-        echo "Hello $name";
-    ?>
-</h1>
-    
-</body>
-</html>
+// POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["opt"])) {
+        // CREATE
+        if ($_POST["opt"] == "create_User")
+            echo json_encode(create_User($_POST["email"], $_POST["password"], $_POST["name"], $_POST["surname"]));
+
+        // UPDATE
+        elseif ($_POST["opt"] == "update_User")
+            echo json_encode(update_User($_POST["id"], $_POST["email"], $_POST["password"], $_POST["name"], $_POST["surname"]));
+
+        // SESSION
+        elseif ($_POST["opt"] == "login")
+            echo json_encode(login($_POST["email"], $_POST["password"], $_POST["remember"]));
+    }
+}
+// GET
+elseif ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if (isset($_GET["opt"])) {
+        // READ
+        if ($_GET["opt"] == "read_All")
+            echo json_encode(read_All());
+
+        elseif ($_GET["opt"] == "read_Specific")
+            echo json_encode(read_Specific($_GET["id"]));
+
+        // DELETE
+        elseif ($_GET["opt"] == "delete_User")
+            echo json_encode(delete_User($_GET["id"]));
+
+        // SESSION
+        elseif ($_GET["opt"] == "log_Out")
+            echo json_encode(log_Out());
+    }
+}
