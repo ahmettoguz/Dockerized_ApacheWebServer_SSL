e8b048717f2d7ca9b765d9b39fd05fed02924b52
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-10-17 02:38:56 +0300

    Cors policy headers added to use as an api.

--- Dynamic_Php_MySql_DigitalOcean/php/index.php
+++ Dynamic_Php_MySql_DigitalOcean/php/index.php
@@ -1,6 +1,9 @@
 <?php
 require_once __DIR__ . "/assets/controller/sql_functions.php";
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
 // POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
