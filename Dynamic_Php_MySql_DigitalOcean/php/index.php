f6f30014c73563b742a6fd8784c6e87d6983b274
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-10-17 09:00:48 +0300

    Cors stage.

--- Dynamic_Php_MySql_DigitalOcean/php/index.php
+++ Dynamic_Php_MySql_DigitalOcean/php/index.php
@@ -5,6 +5,17 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    header('Access-Control-Allow-Headers: *');
+    exit;
+}
+
+
+
+
+
 // POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["opt"])) {
