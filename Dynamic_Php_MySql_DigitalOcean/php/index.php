e3cbe57229c134cc7a4c1804fac8e9e8c35fb387
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-05 22:45:18 +0300

    SSL tirals start.

--- Dynamic_Php_MySql_DigitalOcean/php/index.php
+++ Dynamic_Php_MySql_DigitalOcean/php/index.php
@@ -1,9 +1,14 @@
 <?php
 require_once __DIR__ . "/assets/controller/sql_functions.php";
-header("Content-Type: application/json");
+// header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+echo "<h1> HELLO AHMET </h1>";
+exit;
+
+
 
 // POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
