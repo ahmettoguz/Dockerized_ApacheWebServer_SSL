b42b601e1f980548bbc0ab1ae78190dd3675f52d
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-06 00:19:17 +0300

    .

--- Dynamic_Php_MySql_DigitalOcean/php/index.php
+++ Dynamic_Php_MySql_DigitalOcean/php/index.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
-getenv("MY_NAME");
+echo getenv("MY_NAME");
 
 // POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
