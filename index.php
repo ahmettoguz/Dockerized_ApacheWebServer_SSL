a1960fd3f58a4cf3071c702d444b3e959b520d93
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-19 01:07:04 +0300

    Domain in progress.

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        h1 {
+            text-align: center;
+        }
+    </style>
 </head>
-<body>
 
-<h1>
-    <?php
-        $name = getenv("MY_NAME");
-        echo "Hello $name";
-    ?>
-</h1>
-    
+<body>
+    <h1>
+        <?php
+        $app = getenv("APP_NAME");
+        echo $app;
+        ?>
+    </h1>
 </body>
+
 </html>
