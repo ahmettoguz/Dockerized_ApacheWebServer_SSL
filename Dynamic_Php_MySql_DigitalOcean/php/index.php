685fff3b2124cd74027d36cf8fd75c3c1e189cb9
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-18 12:21:58 +0300

    Return to static website

--- Dynamic_Php_MySql_DigitalOcean/php/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . "/assets/controller/sql_functions.php";
-// header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-
-echo "<h1>" . getenv("MY_NAME") . "</h1>";
